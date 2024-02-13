d2883115d5aea5c61116c074e0295c4b553b40e4
sttkm <st_tkm@websci.cs.tsukuba.ac.jp>
2024-02-14 00:44:53 +0900

    fix

---
+++ ga.sh
@@ -0,0 +1,9 @@
+
+python run_evogym_ga.py -n Climber-v0_ga_0 -t Climber-v0
+python run_evogym_ga.py -n Flipper-v0_ga_0 -t Flipper-v0
+python run_evogym_ga.py -n Thrower-v0_ga_0 -t Thrower-v0
+python run_evogym_ga.py -n PlatformJumper-v0_ga_0 -t PlatformJumper-v0
+python run_evogym_ga.py -n Hurdler-v0_ga_0 -t Hurdler-v0
+python run_evogym_ga.py -n Lifter-v0_ga_0 -t Lifter-v0
+python run_evogym_ga.py -n Carrier-v0_ga_0 -t Carrier-v0
+python run_evogym_ga.py -n BeamSlider-v0_ga_0 -t BeamSlider-v0
